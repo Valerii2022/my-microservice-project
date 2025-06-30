082270801a3ceb8daf20966f8fe3ee99cc02ee5a
Valerii <vmpometun@gmail.com>
2025-06-30 11:02:15 +0000

    Test terraform AWS work

--- backend.tf
+++ backend.tf
@@ -1,11 +1,10 @@
-
-
 terraform {
   backend "s3" {
     bucket         = "lesson5-terraform-state-bucket"
     key            = "lesson-5/terraform.tfstate"
     region         = "us-west-2"
-    use_lockfile = true
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
+
